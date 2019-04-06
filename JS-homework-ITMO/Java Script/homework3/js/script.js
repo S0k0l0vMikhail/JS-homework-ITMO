10ff78a4f54fc941a9769138c69dd57352085062
Kargozerov <kargozerov@yandex.ru>
2019-04-06 13:49:26 +0300

    ispravlenoe HW-JS3

--- JS-homework-ITMO/Java Script/homework3/js/script.js
+++ JS-homework-ITMO/Java Script/homework3/js/script.js
@@ -42,3 +42,62 @@ function SumRecursia(natN) {
 }
 let natX = SumRecursia(123);
 console.log(parseInt(natX)); //6
+
+console.log("Задача 3");
+//Напишите функцию range, принимающую три аргумента, два обязательных: начало и конец диапазона, третий аргумент - необязательный (если он не задан, шаг равен единице) – шаг для построения массива. Функция возвращает массив, который содержит все числа из него, включая начальное и конечное. Например, вызов функции range(1, 10, 2) должен будет вернуть [1, 3, 5, 7, 9].
+let arrR = [];
+let count = 0;
+
+function range(start, end, step) {
+  if (!step) {
+    step = 1;
+  }
+  if (!start || !end || end < start) {
+    console.log("введите стартовые значения");
+    return;
+  }
+  for (i = start; i <= end; i = i + step) {
+    arrR.push(i);
+  }
+  return arrR;
+}
+console.log(range(1, 10, 2));
+
+
+console.log("Задача 4");
+
+//Напишите функцию, которая в зависимости от переданного в нее целочисленного аргумента count, будет выводить слово «товар» в нужно форме («12 товаров», но «22 товара» и тд).
+ // let countTovar = prompt("введите количесто товаров:");
+let text1;
+let text2;
+let text3;
+
+ function diTovar(n) {
+   if (n%100===11 || n%100===12 ||n%100===13 ||n%100===14){
+     return text3 = (n + " товаров");
+   }
+  if ((n - 1) % 10 === 0 ) {
+    return text1 = (n + " товар");
+      }
+  if ((n - 2) % 10 === 0) {
+    return text2 = (n + " товара");
+
+  }
+  if ((n - 3) % 10 === 0) {
+    return text2 = (n + " товара");
+
+  }
+  if ((n - 4) % 10 === 0) {
+      return text2 = (n + " товара");
+
+  } else {
+    return text3 = (n + " товаров");
+
+  }
+}
+var vvodi = prompt('Добавте товары в корзину', 10);
+
+
+let korzina = diTovar(vvodi);
+
+console.log("У вас в корзине: " + korzina);
